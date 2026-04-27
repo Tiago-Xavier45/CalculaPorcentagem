9c3f8889d52302d73e78ca56fa26a62b8fedc8ba
Tiago-Xavier45 <tiago.xavier45@outlook.com>
2026-04-27 17:41:18 -0300

    arrumanco conexao back/front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = '/api/porcentagem'
+const BASE_URL = import.meta.env.VITE_API_URL + 'porcentagem';
 
 async function post(endpoint, body) {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
